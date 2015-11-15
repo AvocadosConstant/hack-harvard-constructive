134b62e6cbef420476b96b5c9a3c3bf4e4625224
Gabe <gabe1118@yahoo.com>
2015-11-14 22:55:51 -0500

    Run azure (comment mongo)

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -3,19 +3,19 @@ from flask import render_template
 from FlaskWebProject import app
 from flask.ext.pymongo import PyMongo
 import pymongo
-
+'''
 def mongoconn():
     MONGO_URL = "mongodb://gochoa:password1@ds042128.mongolab.com:42128/MongoLab-w"
     connection = pymongo.MongoClient(MONGO_URL)
     db = connection.get_default_database()
     print( db.collection_names())
     return db
-
+'''
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    mongoconn()
+    #mongoconn()
     return render_template(
         'index.html'
     )
