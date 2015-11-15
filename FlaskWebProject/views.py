2cd62feb19d2542bda0724df0bb33e2ec63a3bba
Gabe <gabe1118@yahoo.com>
2015-11-14 21:23:21 -0500

    Take out the trash

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,7 +1,3 @@
-"""
-Routes and views for the flask application.
-"""
-
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
@@ -11,43 +7,5 @@ from FlaskWebProject import app
 def home():
     """Renders the home page."""
     return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
+        'index.html'
     )
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
-
-'''
-# Flask
-from flask import Flask, render_template
-import os
-
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('index.html', template_folder=tmpl_dir)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-'''
