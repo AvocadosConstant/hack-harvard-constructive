4edd1b4116e8582d759c6d199c40b4644163f6cf
Gabe <gabe1118@yahoo.com>
2015-11-14 21:17:29 -0500

    Run test

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,3 +1,42 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from FlaskWebProject import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+'''
 # Flask
 from flask import Flask, render_template
 import os
@@ -11,3 +50,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+'''
